0518abe1f5992d6f53519ff2729706df276b3ee7
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-21 22:08:34 +1300

    context particles

--- code/Sampler.hpp
+++ code/Sampler.hpp
@@ -1,9 +1,11 @@
 #ifndef TwinPeaks_Sampler
 #define TwinPeaks_Sampler
 
+// Includes
 #include <iostream>
 #include <stdexcept>
 #include <stdlib.h>     // For size_t
+#include <tuple>
 #include <vector>
 
 #include "RNG.h"
@@ -27,6 +29,13 @@ class Sampler
         // The particles
         std::vector<ParticleType> particles;
 
+        // The scalars of the particles
+        std::vector<std::tuple<double, double>> scalars;
+
+        // Particles that define the context
+        std::vector<ParticleType> context_particles;
+        std::vector<std::tuple<double, double>> context_scalars;
+
         // Flag for whether the Sampler is ready to go.
         bool initialised;
 
@@ -48,6 +57,9 @@ template<class ParticleType>
 Sampler<ParticleType>::Sampler(size_t num_particles)
 :num_particles(num_particles)
 ,particles(num_particles)
+,scalars(num_particles)
+,context_particles(num_particles)
+,context_scalars(num_particles)
 ,initialised(false)
 {
     if(num_particles == 0)
@@ -57,14 +69,28 @@ Sampler<ParticleType>::Sampler(size_t num_particles)
 template<class ParticleType>
 void Sampler<ParticleType>::initialise(RNG& rng)
 {
+    // Generate the particles
     std::cout<<"# Generating "<<num_particles<<" particles from the prior...";
     std::cout<<std::flush;
+    for(size_t i=0; i<num_particles; ++i)
+    {
+        particles[i].from_prior(rng);
+        scalars[i] = particles[i].get_scalars();
+    }
+    std::cout<<"done."<<std::endl;
 
-    for(ParticleType& particle: particles)
-        particle.from_prior(rng);
+    // Generate the *context* particles
+    std::cout<<"# Generating "<<num_particles<<' ';
+    std::cout<<"context particles from the prior...";
+    std::cout<<std::flush;
+    for(size_t i=0; i<num_particles; ++i)
+    {
+        context_particles[i].from_prior(rng);
+        context_scalars[i] = context_particles[i].get_scalars();
+    }
+    std::cout<<"done."<<std::endl;
 
     initialised = true;
-    std::cout<<"done."<<std::endl;
 }
 
 } // namespace TwinPeaks
